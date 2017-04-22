5772c5d1b1febe6e1be4dd48aaa1c19b06e545ba
finaljam <finaljam.hsu@gmail.com>
2017-04-22 23:55:47 +0800

    push code from PyQt4 to PyQt5, modify type error and update module/api

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from PyQt4 import QtGui QtCore
+from PyQt5 import QtGui, QtCore, QtWidgets
 import sys
 
 
-app = QtGui.QAllplication(sys.argv)
+app = QtWidgets.QApplication(sys.argv)
 
 
 
-class MyWidget(QtGui.QWidget):
+class MyWidget(QtWidgets.QWidget):
   def __init__(self, parent=None):
     super(MyWidget, self).__init__(parent)
     self.__createLayout()
@@ -14,38 +14,37 @@ class MyWidget(QtGui.QWidget):
   def __createLayout(self):
     
     # create element for choose coin name and set buy/sell criterion
-    self.chooseCoin = QtGui.QComboBox()
+    self.chooseCoin = QtWidgets.QComboBox()
     self.chooseCoin.addItem("美金")
     self.chooseCoin.addItem("紐西蘭幣")
     self.chooseCoin.addItem("澳幣")
     self.chooseCoin.addItem("歐元")
     self.chooseCoin.addItem("南非幣")
     
-    self.setBuyValue = QtGui.QLineEdit()
-    self.setSellValue = QtGui.QLineEdit()
+    self.setBuyValue = QtWidgets.QLineEdit()
+    self.setSellValue = QtWidgets.QLineEdit()
     
     # layout for setting coin name and buy/sell criterion
-    self.coinEditLayout = QtGui.QFormLayout()
+    self.coinEditLayout = QtWidgets.QFormLayout()
     self.coinEditLayout.addRow("設定幣別", self.chooseCoin)
     self.coinEditLayout.addRow("設定買入條件", self.setBuyValue)
     self.coinEditLayout.addRow("設定賣出條件", self.setSellValue)
     
     
     
-    
-    
+
     
     
     # Quit Button
-    self.quitBut = QtGui.QPushButton('Quit')
-    self.quitBut.clicked.connect(self.myQuit)
+    self.quitBut = QtWidgets.QPushButton('Quit')
+#    self.quitBut.clicked.connect(self.myQuit)
     
-    self.quitLayout = QtGui.QHBoxLayout()
+    self.quitLayout = QtWidgets.QHBoxLayout()
     self.quitLayout.addWidget(self.quitBut)
     
     
     # Top layout
-    self.topLayout = QtGui.QVBoxLayout()
+    self.topLayout = QtWidgets.QVBoxLayout()
     self.topLayout.addLayout(self.coinEditLayout)
     self.topLayout.addLayout(self.quitLayout)
     
@@ -53,7 +52,7 @@ class MyWidget(QtGui.QWidget):
     
 
     
-wigdet = MyWidget()
-widget.setFixSize(200,270)
-wigdet.show()
+widget = MyWidget()
+widget.setFixedSize(200,270)
+widget.show()
 app.exec_()
